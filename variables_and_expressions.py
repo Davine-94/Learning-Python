6322a2f08c466d9d93e1b0f06d4e3ee36a55c998
davine94 <davine94>
2022-04-25 14:07:08 +0300

    Learning variables

---
+++ variables_and_expressions.py
@@ -0,0 +1,20 @@
+#integers, strings, boolean, dictionaries, sequences
+myint = 4
+myfloat = 3.3
+mystr = "This is my name"
+mybool = False
+mylist = [0,1,2,3,4,5]
+mytuple= (0,1,2)
+mydict= {"one": 1, "two": 2}
+
+
+print (myint)
+print(myfloat)
+print(mystr)
+print(mybool)
+print(mylist)
+print(mytuple)
+print(mydict)
+
+#You can redclare variables
+myint = "this is a new string"
