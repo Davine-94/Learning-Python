f9bd7fc82e77a61320c68125c13fa47e90ae3ece
davine94 <davine94>
2022-04-27 12:25:19 +0300

    Added a new file to learn loops

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,12 +1,20 @@
 #Conditional statements use if, elif, else
-def mathematical1(x, y):
-    x, y = 10, 100
-    
+# def mathematical1(x, y):
+def main():
+    x = 10
+    y = 100
+
     if x < y:
+    
         result = "x is less than y"
     else:
         result = "x is greater than y"
     
     print(result)
+    
+
+if __name__ == "__main__":
+    main()
+
 
 
