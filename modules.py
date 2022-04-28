57a0b1101b733762c820be59b16877b4a0800090
davine94 <davine94>
2022-04-28 14:53:54 +0300

    Learning the use of from to import

---
+++ modules.py
@@ -0,0 +1,19 @@
+# import math
+
+# print("The square root of 81 is", math.sqrt(81))
+# print ("Pi is", math.pi)
+# import time
+# import random
+
+# print(random.randint(20,25))
+
+# timestamp = time.time()
+# current_time = time.ctime(timestamp)
+# print(current_time)
+
+# Using from to import
+from random import choice
+fruits = ["apples","mangoes","pineapples"]
+
+print(choice(fruits))
+
